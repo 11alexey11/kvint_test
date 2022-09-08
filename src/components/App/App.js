7e65a940ba9d97055ae29bcf61b04711bad76ff7
Alexey Yanvarev <yanvarev_2015@mail.ru>
2022-09-08 17:43:07 +0400

    feat: add lottie and dialog window

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,55 @@
 import { useState } from 'react';
 import useMountTransition from '../../hooks/useMountTransition';
+import Lottie from 'react-lottie';
+
+import animationData from '../../lottie/mixture.json';
 
 import './index.scss';
 
 const App = () => {
 	const [isMounted, setIsMounted] = useState(true);
 	const [isFetching, setIsFetching] = useState(false);
-	const shouldRenderChild = useMountTransition(isMounted, 500);
+	const shouldRenderChild = useMountTransition(isMounted, 1000);
 
 	const handleClickButton = () => {
 		setIsMounted(!isMounted);
 		setIsFetching(!isFetching);
+
+		new Promise((resolve) => setTimeout(() => resolve({ code: 200 }), 3000))
+			.then(() => {
+				setIsFetching(false);
+			});
+	};
+
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animationData
 	};
 
 	return (
 		<div className='container'>
 			{
 				shouldRenderChild && (
-					<button className={`btn ${isMounted ? 'appear' : 'disappear'}`} onClick={handleClickButton}>
+					<button className={`btn ${isMounted ? 'appear' : 'disappear'}`} onClick={ handleClickButton }>
 						<span>Click me!</span>
 					</button>
 				)
-			}	
+			}
+			{
+				!shouldRenderChild && isFetching && (
+					<Lottie  isClickToPauseDisabled options={defaultOptions} width={250} height={250} />
+				)
+			}
+			{
+				!shouldRenderChild && !isFetching &&
+				(
+					<div className='dialog appear'>
+						<span className='dialog__bold'>Привет</span>
+						<span>чем могу помочь?</span>
+					</div>
+				)
+			}
 		</div>
 	);
 }
